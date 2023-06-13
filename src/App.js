c86a20b2cf61154f8ef1a4c31fd8ed4f3f19ba01
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-13 12:44:21 +0530

    b1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+// JSX - HTML + Javscript together.
+// Predefined Tag - h1, p
+// JSX: Extented HTML
+// User Defined Tag
+// <App />
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return "hello world";
 }
 
 export default App;
